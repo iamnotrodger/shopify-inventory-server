17ef2ce4aba6e9988eb6fb4fd648c38b63d36f4e
iamnotrodger <rodger.retanal24@gmail.com>
2022-05-18 21:46:18 -0400

    [feature] logging middleware

---
+++ internal/middleware/log.go
@@ -0,0 +1,13 @@
+package middleware
+
+import (
+	"log"
+	"net/http"
+)
+
+func LoggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Println(r.Method, r.RequestURI, r.Host)
+		next.ServeHTTP(w, r)
+	})
+}
