1fa40fda48a66a9ed790f9efae949cec7a4ca0d4
iamnotrodger <rodger.retanal24@gmail.com>
2022-05-19 12:55:43 -0400

    [feature] Add status code and response time in log

--- internal/middleware/log.go
+++ internal/middleware/log.go
@@ -3,11 +3,34 @@ package middleware
 import (
 	"log"
 	"net/http"
+	"time"
 )
+
+type LogResponseWriter struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+func NewLogResponseWriter(w http.ResponseWriter) *LogResponseWriter {
+	return &LogResponseWriter{ResponseWriter: w, statusCode: 200}
+}
+
+func (w *LogResponseWriter) WriteHeader(code int) {
+	w.statusCode = code
+	w.ResponseWriter.WriteHeader(code)
+}
+
+func (w *LogResponseWriter) Write(body []byte) (int, error) {
+	return w.ResponseWriter.Write(body)
+}
 
 func LoggingMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log.Println(r.Method, r.RequestURI, r.Host)
-		next.ServeHTTP(w, r)
+		startTime := time.Now()
+
+		logResponseWriter := NewLogResponseWriter(w)
+		next.ServeHTTP(logResponseWriter, r)
+
+		log.Println(r.Method, r.RequestURI, logResponseWriter.statusCode, time.Since(startTime).String())
 	})
 }
