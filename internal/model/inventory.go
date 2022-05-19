df14cddb327bf4ec63eaff1900e92cd46c882871
iamnotrodger <rodger.retanal24@gmail.com>
2022-05-19 09:42:30 -0400

    [feature] validation function for `inventory` model

--- internal/model/inventory.go
+++ internal/model/inventory.go
@@ -8,3 +8,19 @@ type Inventory struct {
 	Price      float64            `json:"price,omitempty" bson:"price,omitempty"`
 	Warehouses []*Warehouse       `json:"warehouses,omitempty" bson:"warehouses,omitempty"`
 }
+
+func (i *Inventory) Validate() error {
+	if i.Name == "" {
+		return &ErrInvalidModel{
+			message: "inventory requires a name",
+		}
+	}
+
+	if i.Price < 0 {
+		return &ErrInvalidModel{
+			message: "inventory price can't be less than zero",
+		}
+	}
+
+	return nil
+}
