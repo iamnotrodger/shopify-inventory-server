e22957dbab3923236488a17fcc5aa551f13ebb95
iamnotrodger <rodger.retanal24@gmail.com>
2022-05-19 11:33:19 -0400

    [feature] `inventory` `GetBSON`

--- internal/model/inventory.go
+++ internal/model/inventory.go
@@ -10,17 +10,38 @@ type Inventory struct {
 }
 
 func (i *Inventory) Validate() error {
-	if i.Name == "" {
+	if !i.isNameValid() {
 		return &ErrInvalidModel{
 			message: "inventory requires a name",
 		}
 	}
 
-	if i.Price < 0 {
+	if !i.isPriceValid() {
 		return &ErrInvalidModel{
 			message: "inventory price can't be less than zero",
 		}
 	}
 
 	return nil
+}
+
+func (i *Inventory) GetBSON() primitive.M {
+	bson := primitive.M{}
+
+	if i.isNameValid() {
+		bson["name"] = i.Name
+	}
+	if i.isPriceValid() {
+		bson["price"] = i.Price
+	}
+
+	return bson
+}
+
+func (i *Inventory) isNameValid() bool {
+	return i.Name != ""
+}
+
+func (i *Inventory) isPriceValid() bool {
+	return i.Price >= 0
 }
