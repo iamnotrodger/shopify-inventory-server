c42e679849cd743ad99ed6799067528ccb9615ac
iamnotrodger <rodger.retanal24@gmail.com>
2022-05-22 18:34:35 -0400

    [feature][web] `InventoryList` component

---
+++ web/src/components/InventoryList.js
@@ -0,0 +1,24 @@
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import Inventory from './Inventory';
+
+const InventoryList = ({ items = [], children }) => {
+	return (
+		<List>
+			{items.map((inventory, i) => (
+				<Link key={i} to={`inventory/${inventory._id}`}>
+					<Inventory value={inventory} />
+				</Link>
+			))}
+			{children}
+		</List>
+	);
+};
+
+const List = styled.div`
+	display: grid;
+	grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+	gap: 0.75rem;
+`;
+
+export default InventoryList;
