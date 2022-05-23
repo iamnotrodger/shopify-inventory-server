ce93c9366180130313c145544ff1a86fe94e402f
iamnotrodger <rodger.retanal24@gmail.com>
2022-05-22 21:09:59 -0400

    [feature][web] `Header` element

---
+++ web/src/elements/Header.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export default styled.h2`
+	font-family: var(--font-primary);
+	font-size: var(--text-2xl);
+	font-weight: var(--font-bold);
+	line-height: 1.1;
+	margin: 0.5rem 0;
+`;
