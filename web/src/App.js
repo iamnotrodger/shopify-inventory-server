2b7bde13016ab0cdfce6865ef48d16af511bd696
iamnotrodger <rodger.retanal24@gmail.com>
2022-05-22 21:30:59 -0400

    [update][web] Include home page on app

--- web/src/App.js
+++ web/src/App.js
@@ -1,8 +1,18 @@
+import Modal from 'react-modal';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+
+const queryClient = new QueryClient();
+Modal.setAppElement('#root');
+
 const App = () => {
 	return (
-		<div>
-			<h1>Shopify-Inventory</h1>
-		</div>
+		<QueryClientProvider client={queryClient}>
+			<Routes>
+				<Route index element={<HomePage />} />
+			</Routes>
+		</QueryClientProvider>
 	);
 };
 
